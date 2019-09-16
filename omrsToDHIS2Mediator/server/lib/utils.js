1cf82d17e8737b5287c37256fc9c22383e7bb8bd
gilbert <gilbert@savics.org>
2019-09-16 15:33:50 +0100

    add openmrs to dhis2 mediator

--- omrsToDHIS2Mediator/server/lib/utils.js
+++ omrsToDHIS2Mediator/server/lib/utils.js
@@ -1,5 +1,8 @@
 'use strict'
 const URI = require('urijs')
+const moment = require('moment');
+var request = require('request');
+const apiConf = process.env.NODE_ENV === 'test' ? require('../config/test') : require('../config/config')
 
 exports.buildOrchestration = (name, beforeTimestamp, method, url, requestHeaders, requestContent, res, body) => {
   let uri = new URI(url)
@@ -50,13 +53,75 @@ exports.isFineValue = function (value) {
       return false;
     }
   }
-  else if (isNaN(value) == false) {
+  else if ((typeof value) == "number") {
     return true;
   } else {
     return false;
   }
 }
 
+
+exports.isDate = function (value) {
+  if (exports.isString(value) == true) {
+    //2019-09-12T00:00:00.000+0100
+    if (/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}\+\d{4}/.test(value) == true) {
+      return true;
+    } else {
+      return false
+    }
+  } else {
+    return false
+  }
+}
+
+exports.isNumeric = function (value) {
+  return ((typeof value) == "number");
+}
+
+exports.isString = function (value) {
+  return ((typeof value) == "string");
+}
+
+exports.isObject = function (value) {
+  return ((typeof value) == "object");
+}
+
+
+exports.getNewDate = function () {
+  return moment(new Date()).format('YYYY-MM-DD');
+}
+
+exports.convertToDate = function (value) {
+  return moment(value).format('YYYY-MM-DD');
+}
+
+exports.convertToNumber = function (value) {
+  return parseInt(value, 10);
+}
+
+
+exports.getDhis2DropdownValue = function (value, callback) {
+  var options = {
+    url: apiConf.api.dhis2.url + "/api/options.json?&filter=id:eq:" + value + "&fields=code",
+    headers: {
+      'Authorization': 'Basic ' + new Buffer(apiConf.api.dhis2.user.name + ":" + apiConf.api.dhis2.user.pwd).toString('base64'),
+      'Content-Type': 'application/json'
+    }
+  };
+
+  request.get(options, function (error, response, body) {
+    if (error) {
+      callback("");
+    } else {
+      var resp = JSON.parse(body);
+      if (exports.isFineValue(resp) == true && exports.isFineValue(resp.options) == true) {
+        callback(resp.options[0].code);
+      } else {
+        callback("");
+      }
+    }
+  });
+}
 
 exports.buildReturnObject = (urn, status, statusCode, headers, responseBody, orchestrations, properties) => {
   var response = {
