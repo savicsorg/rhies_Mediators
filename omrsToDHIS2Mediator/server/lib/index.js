3f6452ca433104628b882550adcb108a51b1368f
Mamadou Ben TRAORE <tmadben@gmail.com>
2020-01-23 12:14:33 +0000

    Update FOSAID retrieve instruction in index

--- omrsToDHIS2Mediator/server/lib/index.js
+++ omrsToDHIS2Mediator/server/lib/index.js
@@ -500,7 +500,7 @@ var getOrganizationUnit = function (fields, callback) {
     if (utils.isFineValue(fields.location.description) == true && fields.location.description.includes(":") == true) {
 
       //FOSAID: 448 TYPE: CS
-      var labFosaId = fields.location.description.split(":")[1].trim().split(" ")[0].trim();
+      var labFosaId = fields.location.description.split(":")[1].trim().split(" ")[1].trim();
       winston.info('Getting DHIS2 organizationUnit with location fosa id ', labFosaId)
       var options = {
         url: apiConf.api.dhis2.url + "/api/organisationUnits.json?fields=id&&filter=code:eq:" + labFosaId,
