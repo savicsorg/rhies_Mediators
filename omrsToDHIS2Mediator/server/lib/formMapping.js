3d3d94059b90d460f1c6b9cf8617c7f00bc12ccc
Mamadou Ben TRAORE <tmadben@gmail.com>
2020-04-18 12:57:58 +0000

    Add matching dropdown for cbs_Relation du Contact avec le Cas Index

--- omrsToDHIS2Mediator/server/lib/formMapping.js
+++ omrsToDHIS2Mediator/server/lib/formMapping.js
@@ -82,7 +82,19 @@ exports.form1MappingTable = [
   { "7bfac55f-4ae4-4f4a-a597-5584e8be6020": "iTx0txf0FVj" },
   { "e328e0b0-28c3-44c9-9b2a-5f16b5185e2c": "jJxPUCWKW1K" },
   { "c1063a9d-515b-440a-af6a-89375cb44ca0": "y0Z5EVxKowc" },
-  { "3ce84c8e-26fe-102b-80cb-0017a47871b2": "dP9kDCGW6C1" }
+  { "3ce84c8e-26fe-102b-80cb-0017a47871b2": "dP9kDCGW6C1" },
+  { "3ce84b1c-26fe-102b-80cb-0017a47871b2": "SdIpSKZhA6a" },
+  { "78240034-73f3-46d8-b688-81fb99f27056": "mzWU3p77ybU" },
+  { "a71fff61-4db8-43ce-98f8-5de7f689f560": "ABFDAJwKeRZ" },
+  { "e42027db-5008-4cb0-8131-af2d84f59734": "C58YCNuA64x" },
+  { "ca27eadb-c14d-414e-8db9-694b3831e719": "zDFb1kASBZ8" },
+  { "a8415b6a-065d-4cd6-9c70-4cdcec7bf8ef": "gTyCR0HFnjp" },
+  { "f95bdebd-c174-4eaa-86cf-067f78db5364": "c6lPSpoY2T5" },
+  { "825ee96c-5277-4b29-bece-7d94e654da34": "LUcOAQklmNQ" },
+  { "7f0dd8e6-f0c3-4cb1-a81e-24391fc7200b": "S9NJxxEdqyk" },
+  { "98463468-4bc0-4df0-8b78-ad5e208c5d2a": "BRDfNrkhRRW" },
+  { "1394d37f-38ca-4f8a-a486-ac46e0ed7523": "XJERkeIHfcE" },
+  { "d0385b0e-c9ac-4f63-ab8d-b6273c029f9d": "iKzBZiMwvGw" },
 
   
 ];
