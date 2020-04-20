b35adb21f9161418f1e05eafa0080286b24b39aa
Mamadou Ben TRAORE <tmadben@gmail.com>
2020-04-20 08:38:12 +0000

    Revert "correct patientContactNotifierValue in the Json payload"
    This reverts commit 285fc3e85f787f9dee5d026ca4c42e5634b5a1d5.

--- omrsToDHIS2Mediator/server/lib/index.js
+++ omrsToDHIS2Mediator/server/lib/index.js
@@ -1051,7 +1051,7 @@ var addHivCaseBaseSurveillance = function (incomingEncounter, organizationUnit, 
                                   },
                                   {
                                     "dataElement": "GE0hAdM6xMg",
-                                    "value": ""
+                                    "value": patientContactNotifierValue
                                   },
                                   {
                                     "dataElement": "r3DvI1uxJM0",
