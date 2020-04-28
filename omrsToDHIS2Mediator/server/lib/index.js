94341e26a307b66ac27584c5da0efb21edf9df00
Mamadou Ben TRAORE <tmadben@gmail.com>
2020-04-28 13:51:46 +0000

    Ready for testing on 28042020

--- omrsToDHIS2Mediator/server/lib/index.js
+++ omrsToDHIS2Mediator/server/lib/index.js
@@ -2356,8 +2356,8 @@ var addRecencyVL = function (incomingEncounter, organizationUnit, trackedEntityI
 function start(callback) {
   if (apiConf.api.trustSelfSigned) { process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0' }
 
-  //if (apiConf.register) {
-  if (false) {
+  if (apiConf.register) {
+  //if (false) {
     medUtils.registerMediator(apiConf.api, mediatorConfig, (err) => {
       if (err) {
         winston.error('Failed to register this mediator, check your config')
