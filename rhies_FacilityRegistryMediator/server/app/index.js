c07e1c6343a6680d7334693998566ffa46b4714b
Mamadou Ben TRAORE <tmadben@gmail.com>
2020-07-17 22:22:14 +0000

    Update index (folder with ./ instead of ../)
    Update index (folder with ./ instead of ../)

--- rhies_FacilityRegistryMediator/server/app/index.js
+++ rhies_FacilityRegistryMediator/server/app/index.js
@@ -131,8 +131,8 @@ function start(callback) {
 
           // Create and start HTTPS server
               var httpsServer = https.createServer({
-                key: fs.readFileSync('../config/certificates/privkey.pem'),
-                cert: fs.readFileSync('../config/certificates/cert.pem')
+                key: fs.readFileSync('./config/certificates/privkey.pem'),
+                cert: fs.readFileSync('./config/certificates/cert.pem')
             }, app);  
             const server = httpsServer.listen(port, () => {
               if (apiConf.heartbeat) {
@@ -160,8 +160,8 @@ function start(callback) {
 
       // Create and start HTTPS server
      var httpsServer = https.createServer({
-        key: fs.readFileSync('../config/certificates/privkey.pem'),
-        cert: fs.readFileSync('../config/certificates/cert.pem')
+        key: fs.readFileSync('./config/certificates/privkey.pem'),
+        cert: fs.readFileSync('./config/certificates/cert.pem')
     }, app);
       const server = httpsServer.listen(port, () => callback(server));
   
